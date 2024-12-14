fd10822e065a922f058e65eb3139e307d69a5000
Mark Archer <mark_archer@live.com>
2024-12-14 11:49:34 -0800

    chore: Better starting point

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import { IPeersPackage } from "peers-sdk";
 import { helloWorld, helloWorld2 } from "./tool-examples";
 
 const peersPackage: IPeersPackage = {
+  assistants: [],
   toolInstances: [
     helloWorld,
     helloWorld2,
   ],
+  workflows: [],
+  events: [],
+  UIs: [],
 };
 
 // peers requires that you export the package components in this way
