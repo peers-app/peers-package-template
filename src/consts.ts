3ea3f9e8bc405c756c7b32e6d372f7c80179d4c6
Mark Archer <mark_archer@live.com>
2025-08-27 10:34:39 -0700

    chore: Update to newest structure

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,2 @@
-export const packageName = '<package-name>';
-export const packageId = '<package-id>';
+export const packageId = require('../package.json').peers.packageId;
 export const appScreenId = '<app-screen-id>';
--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { registerPeersUI } from "peers-sdk";
-import { appScreenId, packageName } from "../consts";
+import { appScreenId } from "../consts";
 
 function AppScreen() {
   const [count, setCount] = useState(0);
 
   return (
     <div style={{ padding: '20px' }}>
-      <h1>Welcome to {packageName}!</h1>
+      <h1>New Package Screen!</h1>
       <p>You can use this as your main application screen or throw it away.</p>
       <p>Button clicked {count} times.</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
